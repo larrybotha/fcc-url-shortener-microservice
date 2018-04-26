d026a861bbbcf02ea070e20ad36ac0b7f6437511
Glitch (fcc-lb-url-shortener-microservice) <none>
2018-04-26 07:25:44 +0000

    ⚡️💽 Updated with Glitch

--- db.js
+++ db.js
@@ -1,21 +1,17 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const user = process.env.DB_USER;
-const password = process.env.DB_PWORD;
+const urlString = process.env.DB_URL_STRING;
 const dbName = process.env.DB_NAME;
-const host = process.env.DB_HOST;
-const port = process.env.DB_PORT;
 
 // https://zellwk.com/blog/crud-express-mongodb/
 // https://dev.to/aurelkurtula/building-a-restful-api-with-express-and-mongodb--3mmh
 // http://mongodb.github.io/node-mongodb-native/3.0/api/MongoClient.html#.connect
-const url = `mongodb://${user}:${password}@${host}:${port}`;
 let db;
 
-MongoClient.connect(url, (err, client) => {
+MongoClient.connect(`${urlString}/${dbName}`, (err, client) => {
   if (err) throw err;
   
-  db = client.db(dbName);
+  db = client.db();
 })
 
 module.exports = db;
